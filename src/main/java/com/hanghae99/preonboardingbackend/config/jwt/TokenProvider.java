f46864636829e76f00c18fb60408cf9f2d384ca6
Kim Jihyun <legojhk@gmail.com>
2024-07-09 23:31:48 +0900

    DAY2 : JWT 토큰 생성

--- src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
+++ src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
@@ -1,24 +1,84 @@
 package com.hanghae99.preonboardingbackend.config.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-@Configuration
+@Component
 @Slf4j
 public class TokenProvider implements InitializingBean {
     private static final String AUTHORITIES_KEY = "authorities";
+    private static final String BEARER = "bearer";
+    private static final long ACCESS_TOKEN_VALIDITY = 1000 * 60 * 30; // 30분
+    private static final long REFRESH_TOKEN_VALIDITY = 1000 * 60 * 60 * 24 * 7; // 7일
+
     @Value("${jwt.secret}")
     private String secret;
-    @Value("${jwt.token-validity-in-milliseconds}")
-    private long tokenValidityInMilliseconds;
     private Key key;
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
+    public String createAccessToken(String username, Set<String> authorities) {
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + ACCESS_TOKEN_VALIDITY);
+
+        return Jwts.builder()
+            .setSubject(username)
+            .claim(AUTHORITIES_KEY, authorities)
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setIssuedAt(new Date())
+            .setExpiration(validity)
+            .compact();
+    }
+
+    public String createRefreshToken(String username) {
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + REFRESH_TOKEN_VALIDITY);
+
+        return Jwts.builder()
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setIssuedAt(new Date())
+            .setExpiration(validity)
+            .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            log.trace("Invalid JWT token trace: {}", e);
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public Set<String> getAuthoritiesFromToken(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+
+        return ((List<?>) claims.get(AUTHORITIES_KEY)).stream()
+            .map(authority -> (String) authority)
+            .collect(Collectors.toSet());
+    }
+
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
