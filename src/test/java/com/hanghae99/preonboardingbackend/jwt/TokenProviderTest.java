2d1e52b42345ddaf8c591657014756c575cf2c29
Kim Jihyun <legojhk@gmail.com>
2024-07-10 19:09:44 +0900

    DAY2 : 테스트 메서드 Rename, 단위 테스트로 변경

--- src/test/java/com/hanghae99/preonboardingbackend/jwt/TokenProviderTest.java
+++ src/test/java/com/hanghae99/preonboardingbackend/jwt/TokenProviderTest.java
@@ -12,35 +12,33 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class TokenProviderTest {
 
     @InjectMocks
     private TokenProvider tokenProvider;
 
-    @Value("${jwt.secret}")
-    private String secret;
-
     private String invalidToken = "invalidToken";
     String username = "testUser";
-    Set<String> authorities = new HashSet<>();
+    Set<String> authorities;
 
     @BeforeEach
     public void setUp() throws Exception {
-        MockitoAnnotations.openMocks(this);
+        String secret = "a71b8a7edfd427da96e2460b24b6eebe7bd0a6c24aae5a3b0728e843e29f632809a717e6c0825c979def91612c625e70d821a51512fed72ff18aca638a7c4c41";
         ReflectionTestUtils.setField(tokenProvider, "secret", secret);
         tokenProvider.afterPropertiesSet();
+
+        authorities = new HashSet<>();
         authorities.add("ROLE_USER");
     }
 
     @Test
-    public void testCreateAccessToken() {
+    public void testCreateAccessToken_success() {
         //when
         String token = tokenProvider.createAccessToken(username, authorities);
         Set<String> extractedAuthorities = tokenProvider.getAuthoritiesFromToken(token);
@@ -53,7 +51,7 @@ public class TokenProviderTest {
     }
 
     @Test
-    public void testCreateRefreshToken() {
+    public void testCreateRefreshToken_success() {
         //when
         String token = tokenProvider.createRefreshToken();
 
@@ -63,7 +61,7 @@ public class TokenProviderTest {
     }
 
     @Test
-    public void testTokenExpiration() throws InterruptedException {
+    public void testTokenExpiration_success() throws InterruptedException {
         //given
         ReflectionTestUtils.setField(tokenProvider, "accessTokenValidity", 1000);
 
@@ -76,13 +74,13 @@ public class TokenProviderTest {
     }
 
     @Test
-    public void testValidateToken_InvalidToken() {
+    public void testValidateToken_fail_invalidToken() {
         //when - then
         assertFalse(tokenProvider.validateToken(invalidToken));
     }
 
     @Test
-    public void getUsernameFromToken_InvalidToken() {
+    public void testGetUsernameFromToken_fail_invalidToken() {
         //when-then
         assertThrows(io.jsonwebtoken.JwtException.class, () -> {
             tokenProvider.getUsernameFromToken(invalidToken);
